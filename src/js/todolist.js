a1c175bf6547dcbc89abb609b2cb97a00311b8ae
useon <yoosk4986@naver.com>
2022-04-01 17:31:25 +0900

    ✨ 투두리스트 완료시 체크할 수 있는 기능

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -14,12 +14,24 @@ function todolistSubmit(event) {
 function todolistPaint(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
+  span.classList.add('todoCheckBefore');
   span.innerText = newTodo;
   const button = document.createElement('button');
   button.innerText = '❌';
+  button.classList.add('btnCheckBefore');
+  button.addEventListener("click", todolistCheck);
   li.appendChild(span);
   li.appendChild(button);
   todolistMain.appendChild(li);
 }
 
 todolistForm.addEventListener("submit", todolistSubmit);
+
+function todolistCheck(event) {
+  const span = event.target.parentNode.firstChild;
+  const button = event.target;
+  span.classList.remove('todoCheckBefore');
+  button.classList.remove('btnCheckBefore');
+  span.classList.add('todoCheckAfter');
+  button.classList.add('btnCheckAfter');
+}

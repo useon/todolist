2d62c997a9e1288412e25b857185cbc0447bb138
useon <yoosk4986@naver.com>
2022-04-19 01:27:51 +0900

    ✨ 달력 오늘 날짜 하이라이팅 기능

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -14,6 +14,7 @@ let prevLastDate = new Date(year, month, 0).getDate();
 let prevLastDay = new Date(year, month, 0).getDay();
 let thisLastDate = new Date(year, month + 1, 0).getDate();
 let thisLastDay = new Date(year, month + 1, 0).getDay();
+let liToday;
 
 todayView.textContent = `${year}년 ${month + 1}월`;
 
@@ -32,7 +33,11 @@ function daysPut() {
     dateTag += '<li></li>';
   }
   for (let i = 1; i < thisLastDate + 1; i++) {
-    dateTag += `<li>${i}</li>`;
+    if (i == date) {
+      dateTag += `<li><span class="dateHighlighting">${i}</span></li>`;
+    } else {
+      dateTag += `<li>${i}</li>`;
+    }
   }
   dateView.innerHTML = dateTag;
   todayView.textContent = `${year}년 ${month + 1}월`;
@@ -77,6 +82,13 @@ function nextMonthPut() {
   }
   todayView.textContent = `${year}년 ${month + 1}월`;
 }
+
+// function highlighting() {
+//   // 해당 li를 잡아서 선언하기
+//   // 해당 li에 클래스리스트를 부여하기
+//   console.log(liToday, "함수 잘 들어가나요?")
+//   // 잡아내긴 하는데 여기에선 parentNode등을 잡지 못한다.
+// }
 
 prevBtn.addEventListener("click", prevMonthPut);
 todayBtn.addEventListener("click", daysPut);

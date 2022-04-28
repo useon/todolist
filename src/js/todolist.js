66a5fec3928d818eb029606706744222b9a708ac
useon <yoosk4986@naver.com>
2022-04-28 18:16:25 +0900

    🐛 투두리스트 새로고침시 마지막 항목 체크 여부 표시 안되는 버그 수정

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -2,12 +2,43 @@ const todolistForm = document.querySelector('.todolistForm');
 const todolistInput = document.querySelector('.todolistInput');
 const listAddBtn = document.querySelector('.listAddBtn');
 const todolistMain = document.querySelector('.todolistMain')
+
 let todoArr = [];
 const TODOARR_KEY = 'todoArr';
 let isCheck = false;
 let todolistObj;
 let num = 0;
 
+
+
+
+function todolistPaint(newTodo) {
+  const li = document.createElement('li');
+  li.id = newTodo.id;
+  const span = document.createElement('span');
+  const btnCheck = document.createElement('button');
+  const btnRemove = document.createElement('button');
+  btnCheck.classList.add('btnCheck')
+  btnRemove.classList.add('btnRemove');
+  btnCheck.innerText = '';
+  span.classList.add('todoCheckBefore');
+  span.innerText = newTodo.text;
+  btnCheck.addEventListener("click", todolistCheck);
+  btnRemove.innerText = 'X';
+  btnRemove.addEventListener("click", todolistRemove);
+  li.appendChild(btnCheck);
+  li.appendChild(span);
+  li.appendChild(btnRemove);
+  todolistMain.appendChild(li);
+
+  const liArr = document.querySelectorAll('.todolistMain li');
+  console.log(liArr[0].firstChild)
+  for (let i = 0; i < todoArr.length; i++) {
+    if (todoArr[i].check === true && liArr[i] !== undefined) {
+      liArr[i].firstChild.classList.add('btnCheckAfter');
+    }
+  }
+}
 
 function todolistSubmit(event) {
   event.preventDefault();
@@ -20,39 +51,6 @@ function todolistSubmit(event) {
   todoArr.push(todolistObj);
   todolistPaint(todolistObj);
   todolistSave();
-}
-
-function todolistPaint(newTodo) {
-  const li = document.createElement('li');
-  li.id = newTodo.id;
-  const span = document.createElement('span');
-  const btnCheck = document.createElement('button');
-  const btnRemove = document.createElement('button');
-  btnCheck.classList.add('btnCheck')
-  btnRemove.classList.add('btnRemove');
-  li.classList.add('liTodo');
-  btnCheck.innerText = '';
-
-  for (let i = 0; i < todoArr.length; i++) {
-    if (todoArr[i].check === true) {
-      const liArr = document.querySelectorAll('.liTodo');
-      for (let j = 0; j < liArr.length; j++) {
-        if (todoArr[i].id == liArr.item(j).id) {
-          liArr.item(j).firstChild.classList.add('btnCheckAfter');
-        }
-      }
-    }
-  }
-
-  span.classList.add('todoCheckBefore');
-  span.innerText = newTodo.text;
-  btnCheck.addEventListener("click", todolistCheck);
-  btnRemove.innerText = 'X';
-  btnRemove.addEventListener("click", todolistRemove);
-  li.appendChild(btnCheck);
-  li.appendChild(span);
-  li.appendChild(btnRemove);
-  todolistMain.appendChild(li);
 }
 
 todolistForm.addEventListener("submit", todolistSubmit);

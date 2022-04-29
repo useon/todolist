4599604dd498f01a1722a2232b691d027c69d4d7
useon <yoosk4986@naver.com>
2022-04-29 19:00:00 +0900

    💄UI수정: 로컬에 닉네임 저장 유무에 따른 UI변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
+const loginWrap = document.querySelector('.loginWrap');
 const inputNickname = document.querySelector('form');
+const homeBody = document.querySelector('.homeBody');
+const calendar = document.querySelector('.calendar');
+const todolist = document.querySelector('.todolist');
+
+if (localStorage.getItem('nickname') === null || localStorage.getItem('nickname') === '') {
+  homeBody.classList.remove('homeBody');
+  homeBody.classList.add('loginImg');
+} else {
+  visibleHome();
+}
 
 inputNickname.addEventListener('submit', indexNickname);
 
@@ -6,4 +17,14 @@ function indexNickname(event) {
   event.preventDefault();
   const inputValue = inputNickname.children[0].value;
   localStorage.setItem('nickname', JSON.stringify(inputValue));
+  visibleHome();
+}
+
+function visibleHome() {
+  homeBody.classList.remove('loginImg');
+  homeBody.classList.add('homeBody');
+  calendar.classList.remove('invisible');
+  todolist.classList.remove('invisible');
+  loginWrap.classList.add('invisible');
+  loginWrap.classList.remove('loginWrap');
 }
